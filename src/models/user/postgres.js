92176070b0c2c4ccd7590bd65380e89915331266
alexlongerbeam <alexlongerbeam@yahoo.com>
2019-11-08 16:14:40 -0800

    User Model and methods updated

--- src/models/user/postgres.js
+++ src/models/user/postgres.js
@@ -7,10 +7,10 @@ const UserRepo = (postgres) => {
   const createUserTableSQL = `
     CREATE TABLE IF NOT EXISTS users(
       id SERIAL PRIMARY KEY,
-      name text,
+      first_name text,
+      last_name text,
       email text NOT NULL UNIQUE,
-      passhash text NOT NULL,
-      description text,
+      google_id text,
       created_at timestamptz DEFAULT NOW(),
       updated_at timestamptz DEFAULT NOW()
     );`;
@@ -30,13 +30,13 @@ const UserRepo = (postgres) => {
 
   // Inserts a user entry into the users table
   const createUserSQL = `
-    INSERT INTO users(name, email, passhash, description)
+    INSERT INTO users(first_name, last_name, email, google_id)
     VALUES($1, $2, $3, $4)
     RETURNING id, created_at;`;
 
   // Users createUserSQL and inserts a user into the users column. If we get an
   // error, then we return the (null, error), otherwise return (data, null)
-  const createUser = async (name, email, passhash, description) => {
+  const createUser = async (first_name, last_name, email, google_id) => {
     const values = [name, email, passhash, description];
     try {
       const client = await postgres.connect();
@@ -50,81 +50,27 @@ const UserRepo = (postgres) => {
 
   // Retrieve all user fields from the user column by a user's id. This should
   // only ever return one user, since IDs should be unique
-  const getUserByIDSQL = `
-    SELECT id, name, email, passhash, description, created_at, updated_at FROM users WHERE id=$1;`;
+  const getUserIDByGoogleIDSQL = `
+    SELECT id FROM users WHERE google_id=$1;`;
 
-  // Uses getUserByIDSQL to retrieve the user, and return either (user, null),
+  // Uses getUserIDByGoogleID to retrieve the user, and return either (user, null),
   // or (null, error)
-  const getUserByID = async (id) => {
-    const values = [id];
+  const getUserIDByGoogleID = async (google_id) => {
+    const values = [google_id];
     try {
       const client = await postgres.connect();
-      const res = await client.query(getUserByIDSQL, values);
+      const res = await client.query(getUserIDByGoogleID, values);
       client.release();
       return [res.rows[0], null];
     } catch (err) {
       return [null, err];
-    }
-  };
-
-  const getUserByEmailSQL = `
-    SELECT id, name, email, passhash, description, created_at, updated_at FROM users WHERE email=$1;`;
-
-  const getUserByEmail = async (email) => {
-    const values = [email];
-    try {
-      const client = await postgres.connect();
-      const res = await client.query(getUserByEmailSQL, values);
-      client.release();
-      return [res.rows[0], null];
-    } catch (err) {
-      return [null, err];
-    }
-  };
-
-  const updateUserSQL = `
-    UPDATE users
-    SET name=$2, description=$3, updated_at=NOW()
-    WHERE id=$1
-    RETURNING updated_at;`;
-
-  const updateUser = async (id, name, description) => {
-    const values = [id, name, description];
-    try {
-      const client = await postgres.connect();
-      const res = await client.query(updateUserSQL, values);
-      client.release();
-      return [res.rows[0], null];
-    } catch (err) {
-      return [null, err];
-    }
-  };
-
-  // SQL query to delete user by their id
-  const deleteUserSQL = `
-    DELETE FROM users WHERE id=$1;`;
-
-  // Deletes the user, and either returns an error if something went wrong, or
-  // null
-  const deleteUser = async (id) => {
-    const values = [id];
-    try {
-      const client = await postgres.connect();
-      await client.query(deleteUserSQL, values);
-      client.release();
-      return null;
-    } catch (err) {
-      return err;
     }
   };
 
   return {
     setupRepo,
     createUser,
-    getUserByID,
-    getUserByEmail,
-    updateUser,
-    deleteUser,
+    getUserIDByGoogleID,
   };
 };
 
